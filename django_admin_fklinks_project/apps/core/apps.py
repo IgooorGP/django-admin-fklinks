1b260f7c920b2ecaadde5337ff15d905507d467a
Igor Grillo Peternella <igor.feq@gmail.com>
2019-12-31 18:46:44 -0300

    Added utils module. Added core app and the django_admin_fk_links app.

---
+++ django_admin_fklinks_project/apps/core/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CoreConfig(AppConfig):
+    name = "django_admin_fklinks_project.apps.core"
---
+++ django_admin_fklinks_project/apps/django_admin_fklinks/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangoAdminFKLinksConfig(AppConfig):
+    name = "django_admin_fklinks_project.apps.django_admin_fklinks"
